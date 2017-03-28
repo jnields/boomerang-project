26fba40d1a75f0a55387af18687dc77610549525
jnields <jnields+git@gmail.com>
2017-03-28 16:29:49 -0700

    removing warnings

--- webpack.config.client.prod.js
+++ webpack.config.client.prod.js
@@ -102,7 +102,7 @@ module.exports =  {
                 "NODE_ENV": JSON.stringify("production")
             }
         }),
-        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.UglifyJsPlugin({compress:{warnings: false}}),
     ],
     "stats": {
 
